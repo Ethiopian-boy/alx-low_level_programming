d69e433dfd073a1e7fb99b1551a5aa84fb368e05
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-03-10 13:50:02 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-/**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
- */
+
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
