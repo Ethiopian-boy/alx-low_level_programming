ad3da52e3b083e017885074aeb01b03626dc977d
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-03-21 12:30:08 -0700

    string, pointer and array

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: string to convert
+ * Return: int
+ */
+
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int sign = 1;
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+	}
+	sign *= -1;
+	return (n * sign);
+}
