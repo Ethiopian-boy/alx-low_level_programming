56d580517e22af8fe6b5f0600f7808939dd19388
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-03-21 14:28:50 -0700

    string, pointer

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,62 +1,32 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-void randomPasswordGeneration(int N)
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-	int i = 0;
+char password[100];
+	int r, n, i;
 
-	int randomizer = 0;
-
-	srand((unsigned int)(time(NULL)));
-
-	char numbers[] = "0123456789";
-
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-	char symbols[] = "!@#$^&*?";
-
-	char password[N];
-
-	randomizer = rand() % 4;
-
-	for (i = 0; i < N; i++)
+	n = 0;
+	i = 0;
+	srand(time(NULL));
+	while (n < 2645)
 	{
-
-		if (randomizer == 1) 
+		r = rand() % 122;
+		if (r > 32)
 		{
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
+			a[i++] = r;
+			n += r;
 		}
 	}
-	int main(void)
-{
-	int N = 10;
-
-	randomPasswordGeneration(N);
+	a[i++] = (2772 - n);
+	a[i] = '\0';
+	printf("%s", a);
 
 	return (0);
 }
-
