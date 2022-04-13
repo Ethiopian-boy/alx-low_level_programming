ff717fda0d4647177589cf65c01a1a4bc6e7bad2
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-04-13 09:17:11 -0700

    C - Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_name - function that prints a name
- * @name: name of the person
- * @f: input pointer function and execute it
- * Return: nothing
- */
-
+  * print_name - prints a name
+  * @name: input string
+  * @f: function pointer casted to char pointer
+  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != 0 && f != 0)
-		f(name);
+	(*f)(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,28 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
+
 /**
-  * int_index - searches for an integer
-  * @array: pointer to the array
-  * @size: size of the array
-  * @cmp: pointer to the function
-  * Return: index of the first element found
-  */
+ * int_index - function that searches for an integer
+ * @array: input array
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: int
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	i = -1;
-	if (size <= 0 || !(array) || !(cmp))
-		return (-1);
-	while (++i < size)
+	if (array != 0 && cmp != 0)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
+	/* If element not found */
 	return (-1);
 }
