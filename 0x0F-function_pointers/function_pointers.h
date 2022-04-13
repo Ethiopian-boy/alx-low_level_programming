1d67ab752fd1ab886524163d9b175473e66b673a
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-04-13 09:04:02 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef MAINN_H
+#define MAIN_H
+
+typedef __SIZE_TYPE__ size;
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
