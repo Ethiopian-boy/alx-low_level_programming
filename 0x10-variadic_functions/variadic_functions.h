e8f4338ff15f62b7a29c79aeafbb520f08ba98aa
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-04-14 11:25:49 -0700

    C - Variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,26 @@
+#ifndef VAR_H
+#define VAR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct select - create s.
+ * @q: input.
+ * @u: input.
+ *
+ * Return: int
+ *
+ **/
+typedef struct select
+{
+char *q;
+void (*u)(va_list pa);
+} tstruc;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif
