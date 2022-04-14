a5587671de59836e4aa2e8badc10effd92f007d9
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-04-14 14:25:36 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,76 +1,86 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
-/**
-  * p_char - prints characters
-  * @c: character to print
-  */
-void p_char(va_list c)
-{
-	printf("%c", va_arg(c, int));
-}
-/**
-  * p_int - prints integers
-  * @i: integer to print
-  */
-void p_int(va_list i)
-{
-	printf("%d", va_arg(i, int));
-}
-/**
-  * p_float - prints floats
-  * @f: float to print
-  */
-void p_float(va_list f)
-{
-	printf("%f", va_arg(f, double));
-}
-/**
-  * p_string - prints strings
-  * @s: string to print
-  */
-void p_string(va_list s)
-{
-	char *string;
+#include <stdarg.h>
 
-	string = va_arg(s, char *);
-	if (string == NULL)
-		string = "(nil)";
-	printf("%s", string);
+void print_char(va_list arguments);
+void print_int(va_list arguments);
+void print_float(va_list arguments);
+void print_string(va_list arguments);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - print characters
+ * @arguments: input arguments
+ */
+void print_char(va_list arguments)
+{
+	printf("%c", va_arg(arguments, int));
 }
 /**
-  * print_all - prints any argument passed into it
-  * @format: formats symbols in order
-  */
+ * print_int - print integers
+ * @arguments: input arguments
+ */
+void print_int(va_list arguments)
+{
+	printf("%d", va_arg(arguments, int));
+}
+/**
+ * print_float - print floats
+ * @arguments: input arguments
+ */
+void print_float(va_list arguments)
+{
+	printf("%f", va_arg(arguments, double));
+}
+/**
+ * print_string - print strings
+ * @arguments: input arguments
+ */
+void print_string(va_list arguments)
+{
+	char *args = va_arg(arguments, char *);
+
+	if (args == NULL)
+	{
+		printf("%p", args);
+		return;
+	}
+	printf("%s", args);
+}
+/**
+ * print_all - prints anything
+ * @format: input string
+ */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j;
-	char *separator;
-	va_list argp;
-	v_types valid_types[] = {
-		{"c", p_char},
-		{"i", p_int},
-		{"f", p_float},
-		{"s", p_string}
+	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
 	};
 
-	i = j = 0;
-	separator = "";
-	va_start(argp, format);
-	while (format && format[i])
+	va_start(args, format);
+
+	while (format && (*(format + i)))
 	{
 		j = 0;
-		while (j < 4)
-		{
-			if (format[i] == *valid_types[j].valid)
-			{
-				printf("%s", separator);
-				valid_types[j].f(argp);
-				separator = ", ";
-			}
+
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
 			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
 		}
+
 		i++;
 	}
 	printf("\n");
+	va_end(args);
 }
