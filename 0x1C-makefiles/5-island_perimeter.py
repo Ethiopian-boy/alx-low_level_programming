497e32b982217f4615a63124f3abe79f1ddb9ed1
Ethiopian-boy <mullerfromethiopia@gmail.com>
2022-08-17 17:30:13 +0000

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,18 +2,14 @@
 """ Program that discover the perimeter of an island depends on grid """
 
 def island_perimeter(grid):
-    perimeter = 0
-    r = len(grid)
+    r, c = len(grid), len(grid[0])
+    land, nei = 0, 0
     for row in range(r):
-        c = len(grid[row])
         for col in range(c):
             if grid[row][col] == 1:
-                if (row == 0 or grid[row - 1][col] != 1):
-                    perimeter += 1
-                if (row == (r - 1) or (row < r -1 and grid[row + 1][col] != 1)):
-                    perimeter += 1
-                if (col == 0 or (grid[row][col - 1] != 1)):
-                    perimeter += 1
-                if (col == (c -1) or (col < c - 1 and grid[row][col + 1] != 1)):
-                    perimeter += 1
-    return perimeter
+                land += 1
+                if row < r - 1 and grid[row + 1][col] == 1:
+                    nei += 1
+                if col < c - 1  and grid[row][col + 1] == 1:
+                    nei += 1
+    return (4 * land - 2 * nei)
